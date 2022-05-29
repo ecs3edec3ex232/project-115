10532fccfc9ba59f4fcb00ee4e65bf089f8184b7
Emma Remon <90062286+ecs3edec3ex232@users.noreply.github.com>
2022-05-29 16:55:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function Preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+}
+
+function Take_Snapshot(){
+    save('photo_with_lipstick.jpeg');
+}
+
+function modelLoaded(){
+    console.log("poseNet is initialized");
+}
+
+function gotPoses(results){
+    if( results.length > 0){
+        console.log(results);
+        console.log("lipstick x =" + results[0].pose.nose.x);
+        console.log("lipstick y =" + results[0].pose.nose.y);
+    }
+}
